2521e35925d4934e956905f3ca5c7b005bc6696b
tejasholla <tejasholla17@gmail.com>
2024-05-23 10:49:26 +0530

    json
    json theme file

--- Scripts/Get-WindowsAccentColor.ps1
+++
@@ -1,9 +0,0 @@
-function Get-WindowsAccentColor {
-    $accentColor = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\DWM" -Name "AccentColor"
-    $accentColorHex = '{0:X}' -f $accentColor
-    $accentColorHex = $accentColorHex.PadLeft(8, '0')
-    return "#$($accentColorHex.Substring(2, 6))"
-}
-
-$accentColor = Get-WindowsAccentColor
-Write-Output $accentColor
