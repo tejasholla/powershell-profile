39150272b2977deaa77db2bb07e90e04c61c1152
tejasholla <tejasholla17@gmail.com>
2024-05-23 19:20:29 +0530

    Update hw.ps1
    hw

--- Scripts/hw.ps1
+++ Scripts/hw.ps1
@@ -1,101 +1,96 @@
 function Get-RAMType {
-	param (
-		[string]$Type
-	)
+    param (
+        [string]$Type
+    )
 
-	switch ($Type) {
-		0 { return "Unknown" }
-		2 { return "DRAM" }
-		3 { return "Synchronous DRAM" }
-		4 { return "Cache DRAM" }
-		5 { return "EDO" }
-		6 { return "EDRAM" }
-		7 { return "VRAM" }
-		8 { return "SRAM" }
-		9 { return "RAM" }
-		10 { return "ROM" }
-		11 { return "Flash" }
-		12 { return "EEPROM" }
-		13 { return "FEPROM" }
-		14 { return "EPROM" }
-		15 { return "CDRAM" }
-		16 { return "3DRAM" }
-		17 { return "SDRAM" }
-		18 { return "SGRAM" }
-		19 { return "RDRAM" }
-		20 { return "DDR" }
-		21 { return "DDR2" }
-		22 { return "DDR2 FB-DIMM" }
-		24 { return "DDR3" }
-		25 { return "FBD2" }
-		26 { return "DDR4" }
-		default { return "" }
-	}
+    switch ($Type) {
+        0 { return "Unknown" }
+        2 { return "DRAM" }
+        3 { return "Synchronous DRAM" }
+        4 { return "Cache DRAM" }
+        5 { return "EDO" }
+        6 { return "EDRAM" }
+        7 { return "VRAM" }
+        8 { return "SRAM" }
+        9 { return "RAM" }
+        10 { return "ROM" }
+        11 { return "Flash" }
+        12 { return "EEPROM" }
+        13 { return "FEPROM" }
+        14 { return "EPROM" }
+        15 { return "CDRAM" }
+        16 { return "3DRAM" }
+        17 { return "SDRAM" }
+        18 { return "SGRAM" }
+        19 { return "RDRAM" }
+        20 { return "DDR" }
+        21 { return "DDR2" }
+        22 { return "DDR2 FB-DIMM" }
+        24 { return "DDR3" }
+        25 { return "FBD2" }
+        26 { return "DDR4" }
+        default { return "" }
+    }
 }
 
-#system
+# System
 $system = Get-CimInstance CIM_ComputerSystem
 $os = Get-CimInstance CIM_OperatingSystem
 Write-Host "System Health Report" -ForegroundColor Cyan -BackgroundColor DarkGray
 "System:"
-"- Name: " + $system.Name
-"- Manufacturer: " + $system.Manufacturer
-Write-Host "- Model: " -NoNewline
-Write-Host $system.Model -ForegroundColor DarkCyan
-"- OS: $($os.caption), Service Pack: $($os.ServicePackMajorVersion), Version: $($osInfo.Version)"
+"- Name: $($system.Name)"
+"- Manufacturer: $($system.Manufacturer)"
+"- Model: $($system.Model)"
+"- OS: $($os.Caption), Service Pack: $($os.ServicePackMajorVersion), Version: $($os.Version)"
 
-#motherboard
+# Motherboard
 $board = Get-CimInstance -ClassName Win32_BaseBoard
-Write-Host "Motherboard: " -NoNewline
-Write-Host $board.Manufacturer $board.Product -ForegroundColor DarkCyan
+Write-Host "Motherboard: $($board.Manufacturer) $($board.Product)" -ForegroundColor DarkCyan
 
-#cpu
+# CPU
 $cpus = Get-CimInstance CIM_Processor
 Write-Host "CPU: "
-foreach ($cpu in $cpus) {
-	Write-Host "- " -NoNewline
-	Write-Host $cpu.Name -ForegroundColor DarkCyan -NoNewline
-	" (cores $($cpu.NumberOfCores)/$($cpu.NumberOfLogicalProcessors))"
+$cpus | ForEach-Object {
+    "- $_.Name (cores $($_.NumberOfCores)/$($_.NumberOfLogicalProcessors))" -ForegroundColor DarkCyan
 }
 
-#ram
+# RAM
 $rams = Get-CimInstance -ClassName Win32_PhysicalMemory
-"RAM: "
-"- Capacity: {0:N2} GiB" -f (($rams.Capacity | Measure-Object -Sum).Sum / 1GB)
-"- Total Physical Memory: {0:N2} GiB" -f ($system.TotalPhysicalMemory / 1GB)
-"- Memory modelus: "
-foreach ($ram in $rams) {
-	"  - $((Get-RAMType -Type $ram.SMBIOSMemoryType)) [$($ram.BankLabel)/$($ram.DeviceLocator)] ({0:N2} GiB)" -f ($ram.Capacity / 1GB)
+"RAM:"
+$ramInfo = $rams | ForEach-Object {
+    [PSCustomObject]@{
+        Type = (Get-RAMType -Type $_.SMBIOSMemoryType)
+        BankLabel = $_.BankLabel
+        DeviceLocator = $_.DeviceLocator
+        CapacityGiB = [math]::Round($_.Capacity / 1GB, 2)
+    }
 }
+$ramInfo | Format-Table -AutoSize -Property Type, BankLabel, DeviceLocator, CapacityGiB -ForegroundColor DarkCyan
 
-#gpu
+# GPU
 $gpus = Get-CimInstance -ClassName Win32_VideoController
 "GPU:"
-foreach ($gpu in $gpus) {
-	Write-Host "- " -NoNewline
-	Write-Host $gpu.Caption -ForegroundColor DarkCyan
+$gpus | ForEach-Object {
+    "- $_.Caption" -ForegroundColor DarkCyan
 }
 
-#storage
+# Storage
 $disks = Get-Disk
 "Storage:"
-foreach ($disk in $disks) {
-	Write-Host "- " -NoNewline
-	Write-Host $disk.FriendlyName -ForegroundColor DarkCyan -NoNewline
-	" ({0:N2} GiB)" -f ($disk.Size / 1GB) 
+$disks | ForEach-Object {
+    "- $_.FriendlyName ($([math]::Round($_.Size / 1GB, 2)) GiB)" -ForegroundColor DarkCyan
 }
 
-#volumes
-"- Volumes: "
+# Volumes
+"Volumes:"
 $volumes = Get-Volume
-foreach ($vol in $volumes) {
-	"  - Volume ($($vol.FileSystemLabel)): "
-	if (!($null -eq $vol.DriveLetter)) {
-		"    - DriveLetter: $($vol.DriveLetter)"
-	}
-
-	"    - Capacity: {0:N2} GiB" -f ($vol.Size / 1GB)
-	"    - Free Space: {0:P2} ({1:N2} GiB)" -f ($vol.SizeRemaining / $vol.Size), ($vol.SizeRemaining / 1GB)
+$volumes | ForEach-Object {
+    "- Volume ($($_.FileSystemLabel)):"
+    if ($_.DriveLetter) {
+        "  - DriveLetter: $($_.DriveLetter)"
+    }
+    "  - Capacity: $([math]::Round($_.Size / 1GB, 2)) GiB"
+    "  - Free Space: $([math]::Round($_.SizeRemaining / $_.Size, 2) * 100)% ($([math]::Round($_.SizeRemaining / 1GB, 2)) GiB)"
 }
 
 # Firewall status for all profiles (Domain, Private, Public)
@@ -105,10 +100,10 @@ $firewallStatus = Get-NetFirewallProfile | ForEach-Object {
         Enabled = $_.Enabled
     }
 }
-Write-Host "Firewall: "
-$firewallStatus | Format-Table -Property Profile, Enabled -AutoSize
+Write-Host "Firewall:"
+$firewallStatus | Format-Table -Property Profile, Enabled -AutoSize -ForegroundColor DarkCyan
 
 # Antivirus status
-$antivirusStatus = Get-CimInstance -Namespace root/SecurityCenter2 -ClassName AntivirusProduct | Select-Object displayName, productState
-Write-Host "Antivirus: "
-$antivirusStatus | Format-Table -Property displayName, productState -AutoSize 
+$antivirusStatus = Get-CimInstance -Namespace root/SecurityCenter2 -ClassName AntivirusProduct | Select-Object DisplayName, ProductState
+Write-Host "Antivirus:"
+$antivirusStatus | Format-Table -Property DisplayName, ProductState -AutoSize -ForegroundColor DarkCyan
