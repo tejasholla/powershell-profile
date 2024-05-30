790b4ea414c7c909f455f7520e7fde20d7d1aca8
tejasholla <tejasholla17@gmail.com>
2024-05-30 12:24:16 +0530

    Update Microsoft.PowerShell_profile.ps1
    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -732,32 +732,34 @@ function qr {
     try {
         if ($Text -eq "") { $Text = read-host "Enter text or URL" }
         if ($ImageSize -eq "") { $ImageSize = read-host "Enter image size (e.g. 500x500)" }
-    
+
         $ECC = "M" # can be L, M, Q, H
         $QuietZone = 1
         $ForegroundColor = "000000"
         $BackgroundColor = "ffffff"
         $FileFormat = "jpg"
-            if ($IsLinux) {
-                    $PathToPics = Resolve-Path "$HOME/Pictures"
-            } else {
-                    $PathToPics = [Environment]::GetFolderPath('MyPictures')
-            }
-            if (-not(Test-Path "$PathToPics" -pathType container)) {
-                    throw "Pictures folder at 📂$Path doesn't exist (yet)"
-            }
-        $NewFile = "$PathToPics/QR_code.jpg"
-    
-        $WebClient = new-object System.Net.WebClient
-        $WebClient.DownloadFile(("http://api.qrserver.com/v1/create-qr-code/?data=" + $Text + "&ecc=" + $ECC +`
-            "&size=" + $ImageSize + "&qzone=" + $QuietZone + `
-            "&color=" + $ForegroundColor + "&bgcolor=" + $BackgroundColor.Text + `
-            "&format=" + $FileFormat), $NewFile)
-    
+        $IsLinux = $PSVersionTable.PSPlatform -eq 'Unix'
+        
+        if ($IsLinux) {
+            $PathToPics = Resolve-Path "$HOME/Pictures"
+        } else {
+            $PathToPics = [Environment]::GetFolderPath('MyPictures')
+        }
+        
+        if (-not(Test-Path $PathToPics -PathType Container)) {
+            throw "Pictures folder at $PathToPics doesn't exist (yet)"
+        }
+        
+        $NewFile = Join-Path $PathToPics "QR_code.jpg"
+        
+        $WebClient = New-Object System.Net.WebClient
+        $Url = "http://api.qrserver.com/v1/create-qr-code/?data=$Text&ecc=$ECC&size=$ImageSize&qzone=$QuietZone&color=$ForegroundColor&bgcolor=$BackgroundColor&format=$FileFormat"
+        $WebClient.DownloadFile($Url, $NewFile)
+        
         "✔️ saved new QR code image file to: $NewFile"
         exit 0 # success
     } catch {
-        "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+        "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)"
         exit 1
     }
 }
