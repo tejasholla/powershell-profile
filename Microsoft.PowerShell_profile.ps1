3df952a967ce27ec2d2401d510c123a79039f1b9
tejasholla <tejasholla17@gmail.com>
2024-05-30 12:52:27 +0530

    update
    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -727,43 +727,10 @@ function smart {
 }
 
 function qr {
-    param(
-        [string]$Text = "",
-        [string]$ImageSize = ""
-    )
-
-    try {
-        if ($Text -eq "") { $Text = Read-Host "Enter text or URL" }
-        if ($ImageSize -eq "") { $ImageSize = Read-Host "Enter image size (e.g. 500x500)" }
-
-        $ECC = "M" # can be L, M, Q, H
-        $QuietZone = 1
-        $ForegroundColor = "000000"
-        $BackgroundColor = "ffffff"
-        $FileFormat = "jpg"
-
-        if ($PSVersionTable.PSVersion.Platform -eq "Unix") {
-            $PathToPics = "$HOME/Pictures"
-        } else {
-            $PathToPics = [Environment]::GetFolderPath('MyPictures')
-        }
-
-        if (-not (Test-Path -Path $PathToPics -PathType Container)) {
-            throw "Pictures folder at 📂$PathToPics doesn't exist (yet)"
-        }
-
-        $NewFile = "$PathToPics/QR_code.jpg"
-
-        $WebClient = New-Object System.Net.WebClient
-        $WebClient.DownloadFile(("http://api.qrserver.com/v1/create-qr-code/?data=" + $Text + "&ecc=" + $ECC + `
-            "&size=" + $ImageSize + "&qzone=" + $QuietZone + `
-            "&color=" + $ForegroundColor + "&bgcolor=" + $BackgroundColor + `
-            "&format=" + $FileFormat), $NewFile)
-
-        "✔️ saved new QR code image file to: $NewFile"
-    } catch {
-        "⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)"
-    }
+    # Assuming the script is accessible via the URL
+    $scriptPath = "https://raw.githubusercontent.com/tejasholla/powershell-profile/main/Scripts/qr.ps1"
+    $scriptContent = Invoke-RestMethod -Uri $scriptPath
+    Invoke-Expression $scriptContent
 }
 
 function wea3 {
