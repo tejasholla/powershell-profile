af77aa5d59677d1b089d0547e8e2917e6906873b
tejasholla <tejasholla17@gmail.com>
2024-05-31 11:43:09 +0530

    Update Microsoft.PowerShell_profile.ps1
    new zoxide code

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -859,13 +859,15 @@ function Get-Theme {
 Get-Theme
 
 if (Get-Command zoxide -ErrorAction SilentlyContinue) {
-    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+    # Invoke-Expression (& { (zoxide init powershell | Out-String) })
+    Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
 } else {
     Write-Host "zoxide command not found. Attempting to install via winget..."
     try {
         winget install -e --id ajeetdsouza.zoxide
         Write-Host "zoxide installed successfully. Initializing..."
-        Invoke-Expression (& { (zoxide init powershell | Out-String) })
+        # Invoke-Expression (& { (zoxide init powershell | Out-String) })
+        Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
     } catch {
         Write-Error "Failed to install zoxide. Error: $_"
     }
