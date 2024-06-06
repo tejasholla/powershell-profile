ecd0fc6c0e66f2aa14aeaec0961d87237b1d9cc3
tejasholla <tejasholla17@gmail.com>
2024-06-06 14:00:13 +0530

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -259,6 +259,7 @@ function ytdownload {
     $file = 'ytDownloader.py'
     $fullPath = Join-Path $path $file
     $python310Path = Join-Path $env:USERPROFILE 'AppData\Local\Programs\Python\Python310\python.exe'
+    $python312Path = Join-Path $env:USERPROFILE 'AppData\Local\Programs\Python\Python312\python.exe'
     $gitRepo = 'https://github.com/tejasholla/ytDownloader.git'
 
     # Check if the Python file exists
@@ -280,7 +281,22 @@ function ytdownload {
     }
     # Execute the Python script if it exists
     if (Test-Path $fullPath) {
-        & $python310Path $fullPath
+        $pythonPaths = @($python310Path, $python312Path)
+        $pythonExecuted = $false
+        foreach ($pythonPath in $pythonPaths) {
+            if (Test-Path $pythonPath) {
+                try {
+                    & $pythonPath $fullPath
+                    $pythonExecuted = $true
+                    break
+                } catch {
+                    Write-Host "Failed to execute Python script with path $pythonPath: $_"
+                }
+            }
+        }
+        if (-not $pythonExecuted) {
+            Write-Host "Failed to execute Python script with all available Python paths."
+        }
     } else {
         Write-Host "The script file does not exist even after attempting to download. Please check the repository URL and directory permissions."
     }
