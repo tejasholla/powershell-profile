f0da9d914a1c8ae761743099b1ae0c53b3457559
tejasholla <tejasholla17@gmail.com>
2024-06-06 11:59:34 +0530

    ip adress change script

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -480,50 +480,11 @@ function ConvertTo-PrefixLength {
     return $prefixLength
 }
 
-function ipchangefun{
-# Display network adapters to the user
-Write-Host "Available Network Adapters and their Interface Indexes:"
-Get-NetAdapter | Format-Table Name, InterfaceIndex, Status
-
-# Prompt for Interface Index and validate
-$interfaceIndex = $null
-do {
-    $interfaceIndex = Read-Host "Enter the Interface Index from the list above"
-    if (-not $interfaceIndex -or $interfaceIndex -eq '') {
-        Write-Host "Interface Index cannot be empty, please enter a valid number."
-    }
-    if (-not (Get-NetAdapter | Where-Object InterfaceIndex -eq $interfaceIndex)) {
-        Write-Host "Invalid Interface Index entered. Please enter a number from the list above."
-        $interfaceIndex = $null
-    }
-} while (-not $interfaceIndex)
-
-$ipAddress = Read-Host "Enter the new IP Address"
-$subnetMask = Read-Host "Enter the Subnet Mask"
-
-# Check if subnet mask is empty and set default value
-if ([string]::IsNullOrWhiteSpace($subnetMask)) {
-    $subnetMask = "255.255.255.0"
-}
-
-# Convert the subnet mask to prefix length
-$prefixLength = ConvertTo-PrefixLength -SubnetMask $subnetMask
-
-# Remove the existing IP address
-try {
-    Get-NetIPAddress -InterfaceIndex $interfaceIndex | Remove-NetIPAddress -Confirm:$false
-    Write-Host "Existing IP Address(es) removed."
-} catch {
-    Write-Error "Failed to remove existing IP Address. Error: $_"
-}
-
-# Set the IP Address
-try {
-    New-NetIPAddress -InterfaceIndex $interfaceIndex -IPAddress $ipAddress -PrefixLength $prefixLength
-    Write-Host "IP Address has been changed successfully."
-} catch {
-    Write-Error "Failed to change IP Address. Error: $_"
-}
+function ipchange {
+        # Assuming the script is accessible via the URL
+        $scriptPath = "https://raw.githubusercontent.com/tejasholla/powershell-profile/main/Scripts/ip-change.ps1"
+        $scriptContent = Invoke-RestMethod -Uri $scriptPath
+        Invoke-Expression $scriptContent
 }
 
 function iplocate {
