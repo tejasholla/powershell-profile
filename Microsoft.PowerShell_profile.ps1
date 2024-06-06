39058ee1d54172a1eca9adb9e6425f73d277d670
tejasholla <tejasholla17@gmail.com>
2024-06-06 10:01:48 +0530

    Update Microsoft.PowerShell_profile.ps1
    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -790,22 +790,31 @@ function debloat { irm "christitus.com/win" | iex }
 
 function setup {irm "https://github.com/tejasholla/powershell-profile/raw/main/setup.ps1" | iex}
 
-# Shortens a URL using various free URL shortening services.function shorturl {
-    # Assuming the script is accessible via the URL
+# Shortens a URL using various free URL shortening services.
+function shorturl {
+    # Path to the script on GitHub
     $scriptPath = "https://raw.githubusercontent.com/tejasholla/powershell-profile/main/Scripts/short-url.ps1"
-    $scriptContent = Invoke-RestMethod -Uri $scriptPath
+    
+    try {
+        $scriptContent = Invoke-RestMethod -Uri $scriptPath -ErrorAction Stop
+        
+        # Remove BOM if present
+        if ($scriptContent[0] -eq 0xFEFF) {
+            $scriptContent = $scriptContent.Substring(1)
+        }
 
-    # Save the script content to a temporary file
-    $tempFile = [System.IO.Path]::GetTempFileName() + ".ps1"
-    Set-Content -Path $tempFile -Value $scriptContent
+        # Save script content to a temporary file
+        $tempScriptPath = [System.IO.Path]::GetTempFileName() + ".ps1"
+        Set-Content -Path $tempScriptPath -Value $scriptContent
 
-    # Execute the temporary script file
-    & $tempFile
-
-    # Remove the temporary file
-    Remove-Item -Path $tempFile -Force
+        . $tempScriptPath
+        
+        # Cleanup the temporary file
+        Remove-Item -Path $tempScriptPath -Force
+    } catch {
+        Write-Host "⚠️ Error fetching or executing the script: $($_.Exception.Message)" -ForegroundColor Red
+    }
 }
-
 
 function reset-wsl {
     cd $env:LOCALAPPDATA\Packages\CanonicalGroupLimited.Ubuntu_79rhkp1fndgsc\LocalState\
