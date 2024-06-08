be984a115679190f2ad9f4a44c82f5ec3399e026
tejasholla <tejasholla17@gmail.com>
2024-06-08 17:37:16 +0530

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -22,20 +22,106 @@ if (Test-Path($ChocolateyProfile)) {
 Import-Module -Name z
 
 # Enhanced PowerShell Experience
-Set-PSReadLineOption -Colors @{
-    Command = '#f6de4b'
-    Parameter = 'Green'
-    String = 'DarkCyan'
-    Operator = 'Magenta'
-    ContinuationPrompt = '#56B6C2'
-    Error = '#E06C75'
-    Variable = 'Yellow'  
-    Number = 'Red'
-    Type = 'Cyan'
-    Comment = 'DarkCyan'
-    InlinePrediction = '#70A99F'
-}
+#Set-PSReadLineOption -Colors @{
+#    Command = '#f6de4b'
+#    Parameter = 'Green'
+#    String = 'DarkCyan'
+#    Operator = 'Magenta'
+#    ContinuationPrompt = '#56B6C2'
+#    Error = '#E06C75'
+#    Variable = 'Yellow'  
+#    Number = 'Red'
+#    Type = 'Cyan'
+#    Comment = 'DarkCyan'
+#    InlinePrediction = '#70A99F'
+#}
 
+#PS7 Polyfill
+if ($PSEdition -eq 'Desktop') {
+    $isWindows = $true
+    $isLinux = $false
+    $isMacOS = $false
+  }
+
+  if ($PSStyle) {
+    #Enable new fancy progress bar for Windows Terminal
+    if ($ENV:WT_SESSION) {
+      $PSStyle.Progress.UseOSCIndicator = $true
+    }
+  
+    & {
+      $FG = $PSStyle.Foreground
+      $Format = $PSStyle.Formatting
+      $PSStyle.FileInfo.Directory = $FG.Blue
+      $PSStyle.Progress.View = 'Minimal'
+      $PSStyle.Progress.UseOSCIndicator = $true
+      $DefaultColor = $FG.White
+      $Format.Debug = $FG.Magenta
+      $Format.Verbose = $FG.Cyan
+      $Format.Error = $FG.BrightRed
+      $Format.Warning = $FG.Yellow
+      $Format.FormatAccent = $FG.BrightBlack
+      $Format.TableHeader = $FG.BrightBlack
+      $DarkPlusTypeGreen = "`e[38;2;78;201;176m" #4EC9B0 Dark Plus Type color
+      Set-PSReadLineOption -Colors @{
+        Error     = $Format.Error
+        Keyword   = $FG.Magenta
+        Member    = $FG.BrightCyan
+        Parameter = $FG.BrightCyan
+        Type      = $DarkPlusTypeGreen
+        Variable  = $FG.BrightCyan
+        String    = $FG.Yellow
+        Operator  = $DefaultColor
+        Number    = $FG.BrightGreen
+  
+        # These colors should be standard
+        # Command            = "$e[93m"
+        # Comment            = "$e[32m"
+        # ContinuationPrompt = "$e[37m"
+        # Default            = "$e[37m"
+        # Emphasis           = "$e[96m"
+        # Number             = "$e[35m"
+        # Operator           = "$e[37m"
+        # Selection          = "$e[37;46m"
+      }
+    }
+  
+  } else {
+    #Legacy PS5.1 Configuration
+    #ANSI Escape Character
+    $e = [char]0x1b
+    $host.PrivateData.DebugBackgroundColor = 'Black'
+    $host.PrivateData.DebugForegroundColor = 'Magenta'
+    $host.PrivateData.ErrorBackgroundColor = 'Black'
+    $host.PrivateData.ErrorForegroundColor = 'Red'
+    $host.PrivateData.ProgressBackgroundColor = 'DarkCyan'
+    $host.PrivateData.ProgressForegroundColor = 'Yellow'
+    $host.PrivateData.VerboseBackgroundColor = 'Black'
+    $host.PrivateData.VerboseForegroundColor = 'Cyan'
+    $host.PrivateData.WarningBackgroundColor = 'Black'
+    $host.PrivateData.WarningForegroundColor = 'DarkYellow'
+  
+    Set-PSReadLineOption -Colors @{
+      Command            = "$e[93m"
+      Comment            = "$e[32m"
+      ContinuationPrompt = "$e[37m"
+      Default            = "$e[37m"
+      Emphasis           = "$e[96m"
+      Error              = "$e[31m"
+      Keyword            = "$e[35m"
+      Member             = "$e[96m"
+      Number             = "$e[35m"
+      Operator           = "$e[37m"
+      Parameter          = "$e[37m"
+      Selection          = "$e[37;46m"
+      String             = "$e[33m"
+      Type               = "$e[34m"
+      Variable           = "$e[96m"
+    }
+  
+    Remove-Variable e
+  }
+  
 # PSReadLine configuration
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
