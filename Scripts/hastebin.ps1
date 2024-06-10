4cdaa43b4344cbc0715178e177c4d24e6f473e73
tejasholla <tejasholla17@gmail.com>
2024-06-10 09:16:22 +0530

    Update hastebin.ps1

--- Scripts/hastebin.ps1
+++ Scripts/hastebin.ps1
@@ -1,27 +1,23 @@
-function hbfun {
-    if ($args.Length -eq 0) {
-        Write-Error "No file path specified."
-        return
-    }
-    
-    $FilePath = $args[0]
-    
-    if (Test-Path $FilePath) {
-        $Content = Get-Content $FilePath -Raw
-    } else {
-        Write-Error "File path does not exist."
-        return
-    }
-    
-    $uri = "http://bin.christitus.com/documents"
-    try {
-        $response = Invoke-RestMethod -Uri $uri -Method Post -Body $Content -ErrorAction Stop
-        $hasteKey = $response.key
-        $url = "http://bin.christitus.com/$hasteKey"
-        Write-Output $url
-    } catch {
-        Write-Error "Failed to upload the document. Error: $_"
-    }
+if ($args.Length -eq 0) {
+    Write-Error "No file path specified."
+    return
 }
 
-hbfun
+$FilePath = $args[0]
+
+if (Test-Path $FilePath) {
+    $Content = Get-Content $FilePath -Raw
+} else {
+    Write-Error "File path does not exist."
+    return
+}
+
+$uri = "http://bin.christitus.com/documents"
+try {
+    $response = Invoke-RestMethod -Uri $uri -Method Post -Body $Content -ErrorAction Stop
+    $hasteKey = $response.key
+    $url = "http://bin.christitus.com/$hasteKey"
+    Write-Output $url
+} catch {
+    Write-Error "Failed to upload the document. Error: $_"
+}
