c24d8fa1d8123f6e6bf5cf90206e39648f723721
tejasholla <tejasholla17@gmail.com>
2024-06-10 09:22:54 +0530

    update script

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -729,10 +729,29 @@ function shellhere {
 }
 
 function hb {
-    # Assuming the script is accessible via the URL
-    $scriptPath = "https://raw.githubusercontent.com/tejasholla/powershell-profile/main/Scripts/hastebin.ps1"
-    $scriptContent = Invoke-RestMethod -Uri $scriptPath
-    Invoke-Expression $scriptContent
+    if ($args.Length -eq 0) {
+        Write-Error "No file path specified."
+        return
+    }
+    
+    $FilePath = $args[0]
+    
+    if (Test-Path $FilePath) {
+        $Content = Get-Content $FilePath -Raw
+    } else {
+        Write-Error "File path does not exist."
+        return
+    }
+    
+    $uri = "http://bin.christitus.com/documents"
+    try {
+        $response = Invoke-RestMethod -Uri $uri -Method Post -Body $Content -ErrorAction Stop
+        $hasteKey = $response.key
+        $url = "http://bin.christitus.com/$hasteKey"
+        Write-Output $url
+    } catch {
+        Write-Error "Failed to upload the document. Error: $_"
+    }
 }
 
 function debloat {
