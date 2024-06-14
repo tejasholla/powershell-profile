78d5b42d370529a10778c2c05be7059d8a1f1dde
tejasholla <tejasholla17@gmail.com>
2024-06-14 14:15:57 +0530

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -179,13 +179,81 @@ function Update-PowerShell {
 }
 Update-PowerShell
 
-# Admin Check and Prompt Customization
-$isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+#region prompt
+# Shim the oh-my-posh prompt function to:
+#  * add git info to oh-my-posh
+#  * show toasts for long running commands
+#  * add scrollbar mark support
+IncrementProgress "Prompt shim";
+Copy-Item Function:prompt Function:poshPrompt;
 function prompt {
-    if ($isAdmin) { "[" + (Get-Location) + "] # " } else { "[" + (Get-Location) + "] $ " }
+    $previousSuccess = $?;
+    $previousLastExitCode = $global:LASTEXITCODE;
+    $lastCommandSucceeded = !$previousLastExitCode -or $previousSuccess;
+
+    try {
+      $currentPath = ((Get-Location).Path);
+      # oh-my-posh can read environment variables so we put GIT related
+      # info into environment variables to be read by our custom prompt
+      if ($env:OhMyPoshCustomBranchUriCachePath -ne $currentPath) {
+        $env:OhMyPoshCustomBranchUri = Get-GitUri $currentPath;
+        $env:OhMyPoshCustomBranchUriCachePath = $currentPath;
+      }
+    } catch {
+      Write-Host ("Custom POSH env var prompt Error: " + $_);
+    }
+
+    # Apply the [scrollbar marks](https://learn.microsoft.com/en-us/windows/terminal/tutorials/shell-integration).
+    # This notes the start of the prompt
+    Write-Host "`e]133;A$([char]07)" -NoNewline;
+
+    if (!$lastCommandSucceeded) {
+        cmd /c "exit $previousLastExitCode";
+    }
+
+    # Call the wrapped prompt that oh-my-posh created. We use try/catch blocks here
+    # because uncaught errors coming from prompt functions are not easy to debug.
+    try {
+      poshPrompt;
+    } catch {
+      Write-Host ("POSH Prompt Error: " + $_);
+    }
+
+    # This notes the end of the prompt for scrollbar marks.
+    Write-Host "`e]133;B$([char]07)" -NoNewLine;
+
+    # Use toasts to notify of completion of long running commands
+    try {
+      $lastCommandTookALongTime = $false;
+      $lastCommandTime = 0;
+
+      $h = (Get-History);
+      if ($h.length -gt 0) {
+          $lh = $h[$h.length - 1];
+          $lastCommandTime = $lh.EndExecutionTime - $lh.StartExecutionTime;
+          $lastCommandTookALongTime = $lastCommandTime.TotalSeconds -gt 10;
+          if ($lh.ExecutionStatus -eq "Completed" -and $lastCommandTookALongTime) {
+              $status = "Success: ";
+              if (!$lastCommandSucceeded) {
+                $status = "Failed: ";
+              }
+              New-BurntToastNotification -Text $status,($lh.CommandLine);
+          }
+
+          # This notes the end of the command for scrollbar marks
+          if ($lastCommandSucceeded) {
+            Write-Host "`e]133;D`a" -NoNewline;
+          } else {
+            Write-Host "`e]133;D;$gle`a" -NoNewline;
+          }
+      }
+
+    } catch {
+      Write-Host ("CDHistory Prompt Error: " + $_);
+    }
+    $global:LASTEXITCODE = 0;
 }
-$adminSuffix = if ($isAdmin) { " [ADMIN]" } else { "" }
-$Host.UI.RawUI.WindowTitle = "PowerShell {0}$adminSuffix" -f $PSVersionTable.PSVersion.ToString()
+#endregion
 
 function admin {
     if ($args.Count -gt 0) {
@@ -777,24 +845,6 @@ function shorturl {
     # Remove the temporary file
     Remove-Item -Path $tempFile -Force
 }
-
-Function WhoIs {
-    param (
-                    [Parameter(Mandatory=$True,
-                               HelpMessage='Please enter domain name (e.g. microsoft.com)')]
-                               [string]$domain
-            )
-    Write-Host "Connecting to Web Services URL..." -ForegroundColor Green
-    try {
-    #Retrieve the data from web service WSDL
-    If ($whois = New-WebServiceProxy -uri "http://www.webservicex.net/whois.asmx?WSDL") {Write-Host "Ok" -ForegroundColor Green}
-    else {Write-Host "Error" -ForegroundColor Red}
-    Write-Host "Gathering $domain data..." -ForegroundColor Green
-    #Return the data
-    (($whois.getwhois("=$domain")).Split("<<<")[0])
-    } catch {
-    Write-Host "Please enter valid domain name (e.g. microsoft.com)." -ForegroundColor Red}
-    } #end function WhoIs
     
 function reset-wsl {
     cd $env:LOCALAPPDATA\Packages\CanonicalGroupLimited.Ubuntu_79rhkp1fndgsc\LocalState\
