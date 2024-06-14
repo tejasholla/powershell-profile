884549a61f5675d574287fc1393e7800f3d3b26c
tejasholla <tejasholla17@gmail.com>
2024-06-14 14:55:02 +0530

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -179,27 +179,26 @@ function Update-PowerShell {
 }
 Update-PowerShell
 
-# Shim the oh-my-posh prompt function
+# Shim the oh-my-posh prompt function to:
+#  * add git info to oh-my-posh
+#  * show toasts for long running commands
+#  * add scrollbar mark support
 Copy-Item Function:prompt Function:poshPrompt;
 function prompt {
-    # Admin Check and Prompt Customization
-    $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-    if ($isAdmin) { "[" + (Get-Location) + "] # " } else { "[" + (Get-Location) + "] $ " }
-
     $previousSuccess = $?;
     $previousLastExitCode = $global:LASTEXITCODE;
     $lastCommandSucceeded = !$previousLastExitCode -or $previousSuccess;
 
     try {
-        $currentPath = (Get-Location).Path;
-        # oh-my-posh can read environment variables so we put GIT related
-        # info into environment variables to be read by our custom prompt
-        if ($env:OhMyPoshCustomBranchUriCachePath -ne $currentPath) {
-            $env:OhMyPoshCustomBranchUri = Get-GitUri $currentPath;
-            $env:OhMyPoshCustomBranchUriCachePath = $currentPath;
-        }
+      $currentPath = ((Get-Location).Path);
+      # oh-my-posh can read environment variables so we put GIT related
+      # info into environment variables to be read by our custom prompt
+      if ($env:OhMyPoshCustomBranchUriCachePath -ne $currentPath) {
+        $env:OhMyPoshCustomBranchUri = Get-GitUri $currentPath;
+        $env:OhMyPoshCustomBranchUriCachePath = $currentPath;
+      }
     } catch {
-        Write-Host ("Custom POSH env var prompt Error: " + $_);
+      Write-Host ("Custom POSH env var prompt Error: " + $_);
     }
 
     # Apply the [scrollbar marks](https://learn.microsoft.com/en-us/windows/terminal/tutorials/shell-integration).
@@ -213,9 +212,9 @@ function prompt {
     # Call the wrapped prompt that oh-my-posh created. We use try/catch blocks here
     # because uncaught errors coming from prompt functions are not easy to debug.
     try {
-        poshPrompt;
+      poshPrompt;
     } catch {
-        Write-Host ("POSH Prompt Error: " + $_);
+      Write-Host ("POSH Prompt Error: " + $_);
     }
 
     # This notes the end of the prompt for scrollbar marks.
@@ -223,37 +222,35 @@ function prompt {
 
     # Use toasts to notify of completion of long running commands
     try {
-        $lastCommandTookALongTime = $false;
-        $lastCommandTime = 0;
+      $lastCommandTookALongTime = $false;
+      $lastCommandTime = 0;
 
-        $h = (Get-History);
-        if ($h.length -gt 0) {
-            $lh = $h[$h.length - 1];
-            $lastCommandTime = $lh.EndExecutionTime - $lh.StartExecutionTime;
-            $lastCommandTookALongTime = $lastCommandTime.TotalSeconds -gt 10;
-            if ($lh.ExecutionStatus -eq "Completed" -and $lastCommandTookALongTime) {
-                $status = "Success: ";
-                if (!$lastCommandSucceeded) {
-                    $status = "Failed: ";
-                }
-                New-BurntToastNotification -Text ($status + $lh.CommandLine);
-            }
+      $h = (Get-History);
+      if ($h.length -gt 0) {
+          $lh = $h[$h.length - 1];
+          $lastCommandTime = $lh.EndExecutionTime - $lh.StartExecutionTime;
+          $lastCommandTookALongTime = $lastCommandTime.TotalSeconds -gt 10;
+          if ($lh.ExecutionStatus -eq "Completed" -and $lastCommandTookALongTime) {
+              $status = "Success: ";
+              if (!$lastCommandSucceeded) {
+                $status = "Failed: ";
+              }
+              New-BurntToastNotification -Text $status,($lh.CommandLine);
+          }
 
-            # This notes the end of the command for scrollbar marks
-            if ($lastCommandSucceeded) {
-                Write-Host "`e]133;D`a" -NoNewline;
-            } else {
-                Write-Host "`e]133;D;$gle`a" -NoNewline;
-            }
-        }
+          # This notes the end of the command for scrollbar marks
+          if ($lastCommandSucceeded) {
+            Write-Host "`e]133;D`a" -NoNewline;
+          } else {
+            Write-Host "`e]133;D;$gle`a" -NoNewline;
+          }
+      }
 
     } catch {
-        Write-Host ("CDHistory Prompt Error: " + $_);
+      Write-Host ("CDHistory Prompt Error: " + $_);
     }
     $global:LASTEXITCODE = 0;
 }
-$adminSuffix = if ($isAdmin) { " [ADMIN]" } else { "" }
-$Host.UI.RawUI.WindowTitle = "PowerShell {0}$adminSuffix" -f $PSVersionTable.PSVersion.ToString()
 
 function admin {
     if ($args.Count -gt 0) {
