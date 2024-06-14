ffaaac2445193690965673bddcc8a12bdfb64b4a
tejasholla <tejasholla17@gmail.com>
2024-06-14 14:58:02 +0530

    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -179,119 +179,13 @@ function Update-PowerShell {
 }
 Update-PowerShell
 
-# Shim the oh-my-posh prompt function to:
-#  * add git info to oh-my-posh
-#  * show toasts for long running commands
-#  * add scrollbar mark support
-Copy-Item Function:prompt Function:poshPrompt;
+# Admin Check and Prompt Customization
+$isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 function prompt {
-    $previousSuccess = $?;
-    $previousLastExitCode = $global:LASTEXITCODE;
-    $lastCommandSucceeded = !$previousLastExitCode -or $previousSuccess;
-
-    try {
-      $currentPath = ((Get-Location).Path);
-      # oh-my-posh can read environment variables so we put GIT related
-      # info into environment variables to be read by our custom prompt
-      if ($env:OhMyPoshCustomBranchUriCachePath -ne $currentPath) {
-        $env:OhMyPoshCustomBranchUri = Get-GitUri $currentPath;
-        $env:OhMyPoshCustomBranchUriCachePath = $currentPath;
-      }
-    } catch {
-      Write-Host ("Custom POSH env var prompt Error: " + $_);
-    }
-
-    # Apply the [scrollbar marks](https://learn.microsoft.com/en-us/windows/terminal/tutorials/shell-integration).
-    # This notes the start of the prompt
-    Write-Host "`e]133;A$([char]07)" -NoNewline;
-
-    if (!$lastCommandSucceeded) {
-        cmd /c "exit $previousLastExitCode";
-    }
-
-    # Call the wrapped prompt that oh-my-posh created. We use try/catch blocks here
-    # because uncaught errors coming from prompt functions are not easy to debug.
-    try {
-      poshPrompt;
-    } catch {
-      Write-Host ("POSH Prompt Error: " + $_);
-    }
-
-    # This notes the end of the prompt for scrollbar marks.
-    Write-Host "`e]133;B$([char]07)" -NoNewLine;
-
-    # Use toasts to notify of completion of long running commands
-    try {
-      $lastCommandTookALongTime = $false;
-      $lastCommandTime = 0;
-
-      $h = (Get-History);
-      if ($h.length -gt 0) {
-          $lh = $h[$h.length - 1];
-          $lastCommandTime = $lh.EndExecutionTime - $lh.StartExecutionTime;
-          $lastCommandTookALongTime = $lastCommandTime.TotalSeconds -gt 10;
-          if ($lh.ExecutionStatus -eq "Completed" -and $lastCommandTookALongTime) {
-              $status = "Success: ";
-              if (!$lastCommandSucceeded) {
-                $status = "Failed: ";
-              }
-              New-BurntToastNotification -Text $status,($lh.CommandLine);
-          }
-
-          # This notes the end of the command for scrollbar marks
-          if ($lastCommandSucceeded) {
-            Write-Host "`e]133;D`a" -NoNewline;
-          } else {
-            Write-Host "`e]133;D;$gle`a" -NoNewline;
-          }
-      }
-
-    } catch {
-      Write-Host ("CDHistory Prompt Error: " + $_);
-    }
-    $global:LASTEXITCODE = 0;
+    if ($isAdmin) { "[" + (Get-Location) + "] # " } else { "[" + (Get-Location) + "] $ " }
 }
-
-#region clickablepaths
-# This function takes a URI and text, and returns
-# the text formatted with ANSI escape sequence to make
-# a link from that.
-function Format-TerminalClickableString {
-  param(
-    $Uri,
-    $DisplayText);
-
-  $clickableFormatString = "`e]8;;{0}`e\{1}`e]8;;`e\"
-  $formattedString = ($clickableFormatString -F ($Uri,$DisplayText));
-  $formattedString;
-}
-
-# Make the result of dir into clickable links.
-# This is used by ./TerminalClickable.format.ps1xml
-function Format-TerminalClickableFileInfo {
-  [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
-  [OutputType([string])]
-  [CmdletBinding()]
-  param(
-      [Parameter(Mandatory, ValueFromPipeline)]
-      [IO.FileSystemInfo]$FileInfo
-  )
-
-  process {
-    if (Test-Path function:Format-TerminalIcons) {
-      $displayText = Format-TerminalIcons $FileInfo;
-    } else {
-      $displayText = $FileInfo.Name;
-    }
-    Format-TerminalClickableString $FileInfo.FullName $displayText;
-  }
-}
-
-# Run after Terminal-Icons to have both terminal-icons and clickable
-# paths.
-$terminableClickableFormatPath = (Join-Path $PSScriptRoot "TerminalClickable.format.ps1xml");
-Update-FormatData -PrependPath $terminableClickableFormatPath;
-#endregion
+$adminSuffix = if ($isAdmin) { " [ADMIN]" } else { "" }
+$Host.UI.RawUI.WindowTitle = "PowerShell {0}$adminSuffix" -f $PSVersionTable.PSVersion.ToString()
 
 function admin {
     if ($args.Count -gt 0) {
--- profile.ps1
+++ profile.ps1
@@ -5,3 +5,77 @@ function prompt {
 }
 $adminSuffix = if ($isAdmin) { " [ADMIN]" } else { "" }
 $Host.UI.RawUI.WindowTitle = "PowerShell {0}$adminSuffix" -f $PSVersionTable.PSVersion.ToString()
+
+
+# Shim the oh-my-posh prompt function to:
+#  * add git info to oh-my-posh
+#  * show toasts for long running commands
+#  * add scrollbar mark support
+Copy-Item Function:prompt Function:poshPrompt;
+function prompt {
+    $previousSuccess = $?;
+    $previousLastExitCode = $global:LASTEXITCODE;
+    $lastCommandSucceeded = !$previousLastExitCode -or $previousSuccess;
+
+    try {
+      $currentPath = ((Get-Location).Path);
+      # oh-my-posh can read environment variables so we put GIT related
+      # info into environment variables to be read by our custom prompt
+      if ($env:OhMyPoshCustomBranchUriCachePath -ne $currentPath) {
+        $env:OhMyPoshCustomBranchUri = Get-GitUri $currentPath;
+        $env:OhMyPoshCustomBranchUriCachePath = $currentPath;
+      }
+    } catch {
+      Write-Host ("Custom POSH env var prompt Error: " + $_);
+    }
+
+    # Apply the [scrollbar marks](https://learn.microsoft.com/en-us/windows/terminal/tutorials/shell-integration).
+    # This notes the start of the prompt
+    Write-Host "`e]133;A$([char]07)" -NoNewline;
+
+    if (!$lastCommandSucceeded) {
+        cmd /c "exit $previousLastExitCode";
+    }
+
+    # Call the wrapped prompt that oh-my-posh created. We use try/catch blocks here
+    # because uncaught errors coming from prompt functions are not easy to debug.
+    try {
+      poshPrompt;
+    } catch {
+      Write-Host ("POSH Prompt Error: " + $_);
+    }
+
+    # This notes the end of the prompt for scrollbar marks.
+    Write-Host "`e]133;B$([char]07)" -NoNewLine;
+
+    # Use toasts to notify of completion of long running commands
+    try {
+      $lastCommandTookALongTime = $false;
+      $lastCommandTime = 0;
+
+      $h = (Get-History);
+      if ($h.length -gt 0) {
+          $lh = $h[$h.length - 1];
+          $lastCommandTime = $lh.EndExecutionTime - $lh.StartExecutionTime;
+          $lastCommandTookALongTime = $lastCommandTime.TotalSeconds -gt 10;
+          if ($lh.ExecutionStatus -eq "Completed" -and $lastCommandTookALongTime) {
+              $status = "Success: ";
+              if (!$lastCommandSucceeded) {
+                $status = "Failed: ";
+              }
+              New-BurntToastNotification -Text $status,($lh.CommandLine);
+          }
+
+          # This notes the end of the command for scrollbar marks
+          if ($lastCommandSucceeded) {
+            Write-Host "`e]133;D`a" -NoNewline;
+          } else {
+            Write-Host "`e]133;D;$gle`a" -NoNewline;
+          }
+      }
+
+    } catch {
+      Write-Host ("CDHistory Prompt Error: " + $_);
+    }
+    $global:LASTEXITCODE = 0;
+}
