bc3cf3e531c7b846865b6a6996cfa17dcc9aea17
tejasholla <tejasholla17@gmail.com>
2024-06-15 11:50:41 +0530

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -669,6 +669,20 @@ function New-Directory {
 
 function which($name) { Get-Command $name | Select-Object -ExpandProperty Definition }
 
+function imgcomp {
+    # Define the URL of the Python script
+    $url = "https://raw.githubusercontent.com/tejasholla/Tools/main/Image_Compressor/Image_Compressor.py"
+
+    # Define the path to save the downloaded script
+    $scriptPath = "$env:Temp\Image_Compressor.py"
+
+    # Download the Python script
+    Invoke-WebRequest -Uri $url -OutFile $scriptPath
+
+    # Execute the downloaded Python script
+    python $scriptPath
+}
+
 # Recycle bin functions -----------------------------------------------------------------------------------------------------------
 function binop {
     $RecycleBin = (New-Object -ComObject Shell.Application).Namespace(0xA)
