0e61fa3ec65183d34ce48aeeda8671e51166d84f
tejasholla <tejasholla17@gmail.com>
2024-06-27 09:39:26 +0530

    new update alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -241,28 +241,7 @@ function admin {
 Import-Module "gsudoModule"
 
 # Set aliases for quick access --------------------------------------------------------------------------------------------------
-Set-Alias -Name su -Value admin
-Set-Alias sudo gsudo
-Set-Alias li ls
-Set-Alias g git
-Set-Alias ip ipconfig
-Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
-set-Alias anydesk 'C:\Program Files (x86)\AnyDesk\AnyDesk.exe'
-set-Alias task 'C:\WINDOWS\system32\Taskmgr.exe'
-set-Alias cpl 'D:\apps\Control Panel.lnk'
-set-Alias nucommand 'D:\apps\nu_commands.txt'
-Set-Alias nuopen nu
-set-Alias touch Set-FreshFile
-set-Alias ff Find-Files
-set-Alias unzip Expand-File
-set-Alias mkdir New-Directory
-set-Alias root Set-Home
-Set-Alias -Name vi -Value Launch-Nvim
-Set-Alias -Name vim -Value Launch-Nvim
-Set-Alias -Name nvim -Value Launch-Nvim
-Set-Alias hosts EditHosts -option AllScope
-Set-Alias history EditHistory -option AllScope
+irm "https://github.com/tejasholla/powershell-profile/raw/main/setup.ps1" | iex
 
 # Function definitions ------------------------------------------------------------------------------------------------------------
 function profile {
---
+++ alias.ps1
@@ -0,0 +1,22 @@
+Set-Alias -Name su -Value admin
+Set-Alias sudo gsudo
+Set-Alias li ls
+Set-Alias g git
+Set-Alias ip ipconfig
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+set-Alias anydesk 'C:\Program Files (x86)\AnyDesk\AnyDesk.exe'
+set-Alias task 'C:\WINDOWS\system32\Taskmgr.exe'
+set-Alias cpl 'D:\apps\Control Panel.lnk'
+set-Alias nucommand 'D:\apps\nu_commands.txt'
+Set-Alias nuopen nu
+set-Alias touch Set-FreshFile
+set-Alias ff Find-Files
+set-Alias unzip Expand-File
+set-Alias mkdir New-Directory
+set-Alias root Set-Home
+Set-Alias -Name vi -Value Launch-Nvim
+Set-Alias -Name vim -Value Launch-Nvim
+Set-Alias -Name nvim -Value Launch-Nvim
+Set-Alias hosts EditHosts -option AllScope
+Set-Alias history EditHistory -option AllScope
