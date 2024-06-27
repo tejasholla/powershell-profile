941e0c4f92f73084be82440eeb0f42e9b92c3cfa
tejasholla <tejasholla17@gmail.com>
2024-06-27 14:24:37 +0530

    scripts optimization

--- Scripts/remove-features.ps1
+++
@@ -1,4 +0,0 @@
-Get-WindowsOptionalFeature -Online |
-Where-Object -Property 'FeatureName' -In -Value @(
-  'Microsoft-SnippingTool';
-) | Disable-WindowsOptionalFeature -Online -Remove -NoRestart *&gt;&amp;1 &gt;&gt; "$env:TEMP\remove-features.log";
