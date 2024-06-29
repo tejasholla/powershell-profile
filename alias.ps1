e2016db0f5f51941ee34513b0159ed3db47f71ba
tejasholla <tejasholla17@gmail.com>
2024-06-29 11:55:07 +0530

    Update alias.ps1

--- alias.ps1
+++ alias.ps1
@@ -1,5 +1,4 @@
 # Custamize this list based on your requirements. This script will be called in Microsoft.PowerShell_profile
-
 Set-Alias -Name su -Value admin
 Set-Alias sudo gsudo
 Set-Alias li ls
