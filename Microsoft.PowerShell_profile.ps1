daf0fb6de414e52eca913764433fff4a45fd267e
tejasholla <tejasholla17@gmail.com>
2024-07-12 20:07:51 +0530

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -207,19 +207,19 @@ $filteredOutput = $fastfetchOutput | Select-String -Pattern "OS:|Host:|Packages:
 function Beautify-Output($output) {
     foreach ($line in $output) {
         if ($line -match "OS:") {
-            Write-Host "///////   ///////    " $line -ForegroundColor Blue
+            Write-Host "///////   ///////    " -ForegroundColor Blue $line -ForegroundColor Cyan
         }
         elseif ($line -match "Host:") {
-            Write-Host "///////   ///////    " $line -ForegroundColor Blue
+            Write-Host "///////   ///////    " -ForegroundColor Blue $line -ForegroundColor Cyan
         }
         elseif ($line -match "Packages:") {
             Write-Host "                     " $line -ForegroundColor Magenta
         }
         elseif ($line -match "Shell:") {
-            Write-Host "///////   ///////    " $line -ForegroundColor Blue
+            Write-Host "///////   ///////    " -ForegroundColor Blue $line -ForegroundColor Cyan
         }
         elseif ($line -match "Terminal:") {
-            Write-Host "///////   ///////    " $line -ForegroundColor Blue
+            Write-Host "///////   ///////    " -ForegroundColor Blue $line -ForegroundColor Cyan
         }
     }
 }
