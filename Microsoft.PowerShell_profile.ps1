489ed0f186bd33753dfc8d4d029ec27b4a231f9b
tejasholla <tejasholla17@gmail.com>
2024-07-17 12:38:19 +0530

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -139,6 +139,15 @@ Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
 
 # Fzf configuration
 Import-Module PSFzf
+$env:FZF_DEFAULT_OPTS = @"
+  --color=spinner:#f4dbd6,hl:#ed8796
+  --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6
+  --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796
+  --height=60%
+  --layout=reverse
+  --preview='bat -f {}'
+  --preview-window='right:60%'
+"@
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 # CompletionPredictor Configuration
